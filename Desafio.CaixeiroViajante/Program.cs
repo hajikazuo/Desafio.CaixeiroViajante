ef1358e4317f72a054415793b7875c71ccdc9925
Nilton <hajikazuo@outlook.com>
2024-09-23 08:00:02 -0300

    Add project files.

---
+++ Desafio.CaixeiroViajante/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Desafio.CaixeiroViajante
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] distancias = {
+                { 0, 10, 15 },
+                { 10, 0, 20 },
+                { 15, 20, 0 }
+            };
+
+            int cidades = distancias.GetLength(0);
+            bool[] visitado = new bool[cidades];
+
+            int cidadeAtual = 0;
+            visitado[cidadeAtual] = true;
+
+            int custoTotal = 0;
+            string rota = "0 -> ";
+
+            for (int i = 0; i < cidades - 1; i++)
+            {
+                int proximaCidade = -1;
+                int menorDistancia = int.MaxValue;
+
+                for (int j = 0; j < cidades; j++)
+                {
+                    if (!visitado[j] && distancias[cidadeAtual, j] < menorDistancia)
+                    {
+                        menorDistancia = distancias[cidadeAtual, j];
+                        proximaCidade = j;
+                    }
+                }
+
+                custoTotal += menorDistancia;
+                cidadeAtual = proximaCidade;
+                visitado[cidadeAtual] = true;
+                rota += $"{cidadeAtual} -> ";
+            }
+
+            custoTotal += distancias[cidadeAtual, 0];
+            rota += "0";
+
+            Console.WriteLine($"Menor distancia: {rota}  Custo: {custoTotal}");
+        }
+    }
+}
